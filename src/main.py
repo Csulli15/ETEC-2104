8a43ae07d7b75835db334343e4597b627ba9f1e2
Csulli15 <165224741+Csulli15@users.noreply.github.com>
2025-01-28 15:47:49 -0500

    Created randomized templates

--- src/main.py
+++ src/main.py
@@ -1,24 +1,53 @@
 import cherrypy
 import mako.template
+import mako.lookup
 import os.path
+import random
+import quotes
+import datetime
+
+names = ["Alice","Bob","Carol","Dave"]
 
 PYPATH = os.path.dirname(__file__)
+lookup = mako.lookup.TemplateLookup(
+    directories=[os.path.dirname(__file__)]
+)
+
 
 class App:
+
+    @cherrypy.expose
+    def quote(self):
+        q = random.choice(quotes.quotations)
+        t = lookup.get_template("quotes.html")
+        return t.render(quote=q)
+    
     @cherrypy.expose
     def index(self):
         h = mako.template.Template(filename=f"{PYPATH}/../html/home.html")
-        return h.render()
+        q = random.choice(quotes.quotations)
+        n = random.choice(names)
+        return h.render(quote=q, name=n)
 
     @cherrypy.expose
     def test(self):
-        t = mako.template.Template(filename=f"{PYPATH}/../html/test.html")
-        return t.render()
+        a = mako.template.Template(filename=f"{PYPATH}/../html/test.html")
+        return a.render()
 
     @cherrypy.expose
     def posts(self):
         p = mako.template.Template(filename=f"{PYPATH}/../html/posts.html")
-        return p.render()
+        time = []
+        views = []
+        l = 0
+        while (l < 10):
+            x = datetime.timedelta(minutes=random.randrange(8000))
+            hoursago = int( x.seconds / 3600 )
+            minutesago = round( (x.seconds - hoursago*3600)/60 )
+            time.append(f"{x.days} days, {hoursago} hours, and {minutesago} minutes ago")
+            views.append(random.randint(1, 2500))
+            l += 1
+        return p.render(time=time, views=views)
 
     @cherrypy.expose
     def signup(self):
