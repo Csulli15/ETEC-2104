31d0d9cd92fd3bee32f4dd47e78543d5d4d0dfaa
Csulli15 <165224741+Csulli15@users.noreply.github.com>
2025-02-25 15:38:54 -0500

    Created DOM

--- html/mycode.js
+++ html/mycode.js
@@ -8,10 +8,19 @@ function Check(){
     theinput = document.getElementById("password");
     let p = theinput.value;
     
+    good = 0;
+    let temp = document.getElementsByClassName("alert")
+    if (temp.length > 0){
+        temp[0].parentNode.removeChild(temp[0])
+    }
+    let tmp = document.createElement("div");
+    tmp.classList.add("alert");
+    document.body.appendChild(tmp);
+    
     //Fill-in check
     if ( p.length == 0 || b.length == 0 || e.length == 0 || n.length == 0 ) {
-        alert("Please fill in all areas");
-        return
+        tmp.appendChild(document.createTextNode("You have not filled out all necessary information."));
+        good++
     }
     
     //Time check
@@ -19,20 +28,26 @@ function Check(){
     let d = birthday.getTime();
     let day = Date.now();
     if (d > day - 410248800000) {
-        alert("LEAVE, CHILD");
-        return
+        tmp.appendChild(document.createTextNode(" BE GONE CHILD! "));
+        good++
     }
     
     //Email check
     let first = e.indexOf("@");
     if (first == -1 || first == 0 || first == e.length - 1) {
-        alert("Invalid Email");
-        return
+        tmp.appendChild(document.createTextNode(" Invalid email. "));
+        good++
     }
     let second = e.indexOf("@", first + 1);
     if (second > 0) {
-        alert("Invalid Email");
-        return
+        tmp.appendChild(document.createTextNode(" Invalid email. "));
+        good++
+    }
+
+    if (good == 0){
+        tmp.classList.remove("alert");
+        tmp.classList.add("welcome");
+        tmp.appendChild(document.createTextNode("Welcome!"))
     }
     
 }
